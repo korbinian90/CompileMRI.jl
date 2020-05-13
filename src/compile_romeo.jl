6fe11685d647eb426b17443666c875ae2a9d5f68
korbi <korbinian90@gmail.com>
2020-05-13 16:13:46 +0200

    additional test function

--- src/compile_romeo.jl
+++ src/compile_romeo.jl
@@ -1,4 +1,16 @@
-function compile_romeo(path; kw...)
+function compile_romeo(path; app_name, kw...)
     romeopath = joinpath(pathof(RomeoApp), "..", "..")
-    create_app(romeopath, path; kw...)
+    create_app(romeopath, path; app_name=app_name, kw...)
+    test_romeo(path, app_name)
+end
+
+function test_romeo(path, app_name)
+    file = tempname()
+    phasefile = abspath(joinpath(@__DIR__, "..", "test", "data", "small", "Phase.nii"))
+    args = [phasefile, "-o", file]
+    name = app_name * (Sys.iswindows() ? ".exe" : "")
+    romeofile = joinpath(path, "bin", name)
+    @assert isfile(romeofile)
+    cmd = `$romeofile $args`
+    @assert success(run(cmd))
 end
