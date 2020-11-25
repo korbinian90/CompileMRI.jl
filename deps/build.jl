51d3b9d941a47d8fd2bb9fff4bcf79274a6a39f7
korbinian90 <korbinian.eckstein@gmail.com>
2020-11-25 12:48:50 +0100

    download romeo before testing instead of buildscript

--- deps/build.jl
+++
@@ -1,3 +0,0 @@
-using Pkg
-Pkg.develop(PackageSpec(path="https://github.com/korbinian90/RomeoApp.jl"))
-Pkg.instantiate()
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using CompileMRI
 using Test
 
+if !isdir(romeopath)
+    download_pkg("RomeoApp")
+end
+
 @testset "CompileMRI.jl" begin
     include("romeo_test.jl")
     include("compile_romeo_test.jl")
