e0291ec28f1ad2f012654d7bcc5b37472f3dad3c
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2021-06-26 12:54:00 +0200

    add example call

--- matlab/example_ROMEO_call.m
+++ matlab/example_ROMEO_call.m
@@ -1,12 +1,20 @@
+%% Required
+addpath('NIfTI_20140122')
+phase = load_nii(fn_phase).img;
 
-parameters.TE = headerAndExtraData.te;
-parameters.no_unwrapped_output = ~algorParam.unwrap.isSaveUnwrappedEcho;
-parameters.calculate_B0 = true;
-parameters.mag = headerAndExtraData.magn;
-parameters.phase_offset_correction = 'bipolar';
-parameters.mask = mask;
-parameters.voxel_size = voxelSize; % for MCPC-3D-S phase offset smoothing
-parameters.output_dir = fullfile(tempdir, 'romeo_tmp');
+%% Optional
+parameters.output_dir = fullfile(tempdir, 'romeo_tmp'); % if not set pwd() is used
+parameters.TE = [1,2,3]; % required for multi-echo
+parameters.mag = load_nii(fn_mag).img;
+parameters.mask = load_nii(fn_mask).img; % can be an array or a string: 'nomask' | 'robustmask'
+parameters.calculate_B0 = true; % optianal B0 calculation for multi-echo
+parameters.phase_offset_correction = 'bipolar'; % options are: 'off' | 'on' | 'bipolar'
+parameters.voxel_size = [0.3, 0.3, 1.2]; % for MCPC-3D-S phase offset smoothing
+parameters.additional_flags = '--verbose -q'; % settings are pasted directly to ROMEO cmd (see https://github.com/korbinian90/ROMEO for options)
+
+%% Suggested steps
 mkdir(parameters.output_dir);
 
-[unwrapped, B0] = ROMEO(wrappedField, parameters);
+[unwrapped, B0] = ROMEO(phase, parameters);
+
+%rmdir(parameters.output_dir, 's') % remove the temporary ROMEO output folder
