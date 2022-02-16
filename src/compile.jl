15a0074b7d2f0ec983c9ecf006e8654c76080ea6
Korbinian Eckstein <korbinian90@gmail.com>
2022-02-16 18:40:34 +0100

    remove mkl stuff

--- src/compile.jl
+++ src/compile.jl
@@ -2,7 +2,6 @@ function compile(path="compiled";
         apps = ["romeo", "clearswi"],
         filter_stdlibs=true,
         precompile_execution_file=abspath(joinpath(@__DIR__, "..", "test", "clearswi_test.jl")),
-        clean=false,
         include_transitive_dependencies=false,
         kw...)
 
@@ -13,14 +12,9 @@ function compile(path="compiled";
     apppath = joinpath(dirname(@__DIR__), "App")
     executables=[c=>c for c in apps]
     create_app(apppath, path; executables, filter_stdlibs, precompile_execution_file, include_transitive_dependencies, kw...)
-    copy_mkl(path)
 
     for app in apps
         test(path, app)
-    end
-
-    if clean
-        clean_app(path, apps) # remove unneccesary artifacts dir (600MB)
     end
 
     copy_matlab(path)
--- src/utility.jl
+++ src/utility.jl
@@ -44,52 +44,8 @@ end
 
 pathof(app) = normpath(homedir(), ".julia/dev", app)
 
-function clean_app(path, app_names)
-    # remove all dlls in mkl artifact but keep
-    # mkl_core.1.dll and mkl_rt.1.dll
-
-    artifact_path = joinpath(path, "share", "julia", "artifacts")
-    if !ispath(artifact_path)
-        artifact_path = joinpath(path, "artifacts")
-    end
-    
-    mkl_path = findartifactpath(artifact_path, "mkl")
-    if isnothing(mkl_path) return end
-    if isdir(joinpath(mkl_path, "bin"))
-        for f in readdir(joinpath(mkl_path, "bin"); join=true)
-            if !(occursin("mkl_core.1.dll", f) || occursin("mkl_rt.1.dll", f))
-                rm(f)
-            end
-        end
-    end
-    if isdir(joinpath(mkl_path, "lib"))
-        for f in readdir(joinpath(mkl_path, "lib"); join=true)
-            if !(occursin("libmkl_core.so", f) || occursin("libmkl_rt.so", f))
-                rm(f)
-            end
-        end
-    end
-
-    for app_name in app_names
-        try
-            test_romeo(path, app_name)
-        catch
-            @warn("Artifact cleaning failed! Please recompile clearswi with the option `clean=false`. The artifacts folder will be very large but some of them might not needed and can be manually removed.")
-        end
-    end
-end
-
 function copy_matlab(path)
     cp(joinpath(dirname(@__DIR__), "matlab"), joinpath(path, "matlab"))
-end
-
-function copy_mkl(path)
-    mkl_sha1_str = "28c8373199bf79bd94ce76e3f45eeaef6d9c1c47"
-    mkl_sha1 = Base.SHA1(mkl_sha1_str)
-    if Pkg.artifact_exists(mkl_sha1)
-        destination_artifacts_path = joinpath(path, "share", "julia", "artifacts", mkl_sha1_str)
-        cp(Pkg.artifact_path(mkl_sha1), destination_artifacts_path)
-    end
 end
 
 function update()
