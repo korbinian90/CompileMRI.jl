5d17a9e0b1d7d8f47547cda44831ef739d3dde1f
Korbinian Eckstein <korbinian90@gmail.com>
2022-09-21 04:56:41 +1000

    test chriselrod idea CI

--- src/CompileMRI.jl
+++ src/CompileMRI.jl
@@ -1,6 +1,8 @@
 module CompileMRI
 
     using Pkg, PackageCompiler
+    using HostCPUFeatures, Static
+    HostCPUFeatures.has_feature(::Val{:x86_64_bmi}) = static(false)
 
     include("utility.jl")
     include("compile.jl")
