78da0b88956343efae462f1b9ed8dcfef5b1df44
Korbinian <korbinian.eckstein@gmail.com>
2024-09-18 13:33:18 +0200

    compile new version

--- App/src/App.jl
+++ App/src/App.jl
@@ -13,7 +13,7 @@ import .Mcpc3dsApp: mcpc3ds_main
 import .HomogeneityCorrection: makehomogeneous_main
 import .RomeoMasking: romeo_mask_main
 
-const version = "4.0.6"
+const version = "4.3.0"
 
 function romeo()::Cint
     try
